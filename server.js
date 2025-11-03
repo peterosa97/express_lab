35a02f52b32552fff1af5730e40de9b5146c8b73
Pete Rosa <peterosa97@gmail.com>
2025-11-03 13:42:05 -0500

    added ejs and more things to run this

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express');
+const express = require('express'); //building an app\
 const app = express(); //Calling this function sets up a server
+app.set('view engine', 'ejs');
 
 
-
-app.get('/', (req, res)=> {
+app.get('/', (req, res)=>{
     console.log('Here'); // request
-    res.send("Hello World!"); //response
+    res.render("index");
+   
+    // res.status(500).send("Hello World!"); //response wiht harding coding status
+    //res.download('server.js');
+
 
 }); // this function will run when someone goes to the root fldr 
 
